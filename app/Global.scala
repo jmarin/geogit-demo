85e750a90ecaabdeb6d979dad1e1e3b9179b5814
Juan Marin Otero <juan.marin.otero@gmail.com>
2014-02-07 22:04:25 -0500

    Hook up GeoGit Server

--- app/Global.scala
+++ app/Global.scala
@@ -1,16 +1,44 @@
+import play.api.Play
 import play.api.{ Application, GlobalSettings }
 import play.Logger
 import play.api.mvc.WithFilters
 import play.filters.gzip.GzipFilter
 import org.geogit.server.core.GeoGitFrontEnd
+import akka.actor.{ ActorSystem, Props }
+import akka.io.IO
+import spray.can.Http
+import com.typesafe.config.ConfigFactory
+import scala.concurrent.ExecutionContext.Implicits.global
+
+package object globals {
+
+}
 
 object Global extends WithFilters(new GzipFilter()) with GlobalSettings {
 
-  override def onStart(app: Application) {
+  val frontendPort = ConfigFactory.load().getString("geogit-server-frontend.port")
 
-    Logger.info("GeoGit Server starting")
-    GeoGitFrontEnd.main(Seq("2551").toArray)
+  val config =
+    ConfigFactory.parseString(s"akka.remote.netty.tcp.port=${frontendPort}").withFallback(
+      ConfigFactory.parseString("akka.cluster.roles = [frontend]")).
+      withFallback(ConfigFactory.load())
+
+  implicit val system = ActorSystem("geogitserver", config)
+
+  override def onStart(app: Application) {
+    if (play.api.Play.isDev(play.api.Play.current)) {
+
+      Logger.info("GeoGit Server starting")
+      val frontend = system.actorOf(Props[GeoGitFrontEnd], name = "frontend")
+      IO(Http) ! Http.Bind(frontend, interface = "0.0.0.0", port = config.getInt("geogit-server.port"))
+    }
 
   }
 
+  override def onStop(app: Application) {
+    if (play.api.Play.isDev(play.api.Play.current)) {
+      system.shutdown
+    }
+  }
 }
+
